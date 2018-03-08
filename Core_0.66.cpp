79f164964104850e48fb00babd3eb3e79e060a34
Piotr Musielski <piotrmusielski@mail.com>
2018-03-08 22:33:12 +0100

    Minor bugfixes 0.66

--- Core_0.66.cpp
+++ Core_0.66.cpp
@@ -1,12 +1,13 @@
-// 0.01 version - still in development, does not provide all artifacts (warm machines, misc5), nor secondary skills.
-// 0.10 update - Creatures maxcap is 255, Only red player, only one map mode, the input file is generated manually.
-// 0.20 update - Read and write in one function
-// 0.30 update - Both players in one loop
+// version still in development, does not provide all artifacts (warm machines, misc5), nor secondary skills.
+//  Creatures maxcap is 255, Only red player, only one map mode, the input file is generated manually.
+// 0.20 update Read and write in one function
+// 0.30 update Both players in one loop
 // 0.31 update - code is more clear and everything is buffered in INPUT DATA to an internal buffer.
 // 0.40 update - added every aspect of hero and offsets. Now only Hero type has to be set (line 86). Works for 1 hero only for now.
 // 0.50 update - Working with classes, easy manipulation of input data, offsets, possibility of adding other heroes as well, no spells.
 // 0.60 update - EVERYTHING WORKS FOR BOTH PLAYERS! 
 // 0.65 update - code is now clear with notes for future devs
+// 0.66 update - bugfixes
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -24,7 +25,7 @@ using namespace std;
 class gracz {
 
 public:
-	gracz(int player_number, FILE* input_file) {				/// player_number starting from 0, input file
+	gracz(int player_number, FILE* input_file) {									/// player_number starting from 0, input file
 		getdata(player_number, input_file);											/// constructor - automatically get data from input file						
 	}
 
@@ -35,19 +36,20 @@ private:
 
 	// Dedinition of locations in map
 	int loc[2] = { 9887,9740 };
-	int offset[12] = { 0,9,10,26,28,56,57,90,92,93,110,100 };		// #DEFINE offsets - locations of each element in the map memory
-	int jump[12] = { 1,2,2,4,4,2,2,1,1,1,1,1 };				// #DEFINE jumps - to overcome problem of overwriting, program is "jumping" over the map memory
+	int offset[12] = { 0,9,10,26,28,56,57,90,92,93,110,100 };						//	#DEFINE offsets - locations of each element in the map memory
+	int jump[12] = { 1,2,2,4,4,2,2,1,1,1,1,1 };										//	#DEFINE jumps	- to overcome problem of overwriting, program is "jumping" over the map memory
 
 	/// Definition of the input file
 	FILE * input;
-	int icount[12] = { 1, 8, 8, 7, 7, 16, 16, 2, 1, 1, 4, 72 };		/// Number of elements		(1 hero_name, 7 creatures' type, 7 creatures' amount, 72 spells)
-	int	isize[12] =	 { 3, 3, 1, 3, 5,  3,  3, 3, 3, 3, 3,  1 };	/// Amount of signs per element	(127 heroes = 3 signs, maximum 30 000 creatures -  5 signs, 1 spell bool 0/1 )
-
+	int icount[12] = { 1, 8, 8, 7, 7, 16, 16, 2, 1, 1, 4, 72 };						/// Number of elements			(1 hero_name, 7 creatures' type, 7 creatures' amount, 72 spells)
+	int	isize[12] =	 { 3, 3, 1, 5, 5,  3,  3, 3, 3, 3, 3,  1 };						/// Amount of signs per element	(127 heroes = 3 signs, maximum 30 000 creatures -  5 signs, 1 spell bool 0/1 )
+	
 	/// Definition for output file
 	FILE* output;
-	int ecount[12] = { 1, 8, 8, 7, 7, 16, 16, 2, 1, 1,4, 9};		/// Number of output elements	(the difference is in spells, since the output is 9 spell sums, not 72 bools)						
-	int buffor[64][12];																/// output buffor 8 players * 8 heroes * 12 parameters per hero
+	int ecount[12] = { 1, 8, 8, 7, 7, 16, 16, 2, 1, 1,4, 9};						/// Number of output elements	(the difference is in spells, since the output is 9 spell sums, not 72 bools)						
+	int buffor[64][20];																/// output buffor 8 players * 8 heroes * 12 parameters per hero
 	int hero_no;																	/// Number of hero
+	int wsize[12] = { 1,1,1,2,2,1,1,1,1,1,1,1};
 	
 };
 
@@ -59,7 +61,7 @@ void gracz::getdata(int n, FILE* m)
 	/// LOCAL VARIABLES 
 	int herloc = n * 283;															/// Offset in input file reading
 	char buffer[20];																/// Internal buffer for file reading (max 16 signs)
-	int spell_value[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };					/// Spell values
+	int spell_value[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };							/// Spell values
 	int spellbuffor[9] = { 0 };														/// Subbuffor - for counting the spell sum
 
 
@@ -68,13 +70,15 @@ void gracz::getdata(int n, FILE* m)
 		for (int j = 0; j < icount[i]; j++) {
 			fgets(buffer, isize[i] + 1, input);										/// Save isize of elements into temporary buffer
 			buffor[i][j] = atoi(buffer);											/// Load data to object's buffor elements
+			cout << "buffor " << i << ", " << j+1 << " : " << buffor[i][j] << endl;
 		}
+		
 	}
 
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 8; j++) {
 			fgets(buffer, 2, input);												/// Get data of spells
-			spellbuffor[i] = spellbuffor[i] + atoi(buffer)* spell_value[j];		/// Calculate spell sum
+			spellbuffor[i] = spellbuffor[i] + atoi(buffer)* spell_value[j];			/// Calculate spell sum
 		}
 		buffor[11][i] = spellbuffor[i];												/// Load spell sum as the last element
 	}
@@ -90,9 +94,9 @@ void gracz::write(FILE *output_file) {
 	{
 		for (int i = 0; i < ecount[r]; i++)
 		{
-			int locm = k + offset[r] + i * jump[r];			/// point in the map memory to be overwritten
+			int locm = k + offset[r] + i * jump[r];									/// point in the map memory to be overwritten
 			stringstream sstream;
-			sstream << std::hex << buffor[r][i];			/// change into hex
+			sstream << std::hex << buffor[r][i];									/// change into hex
 			string hexx = sstream.str();
 
 			if (hexx.size() == 4) {													/// switch hex values (as the processor reats bytes backwards)
@@ -117,7 +121,7 @@ void gracz::write(FILE *output_file) {
 
 			else {
 				fseek(output, locm, 0);
-				fwrite(&buffor[r][i], 1, 1, output);
+				fwrite(&buffor[r][i], 1, wsize[r], output);
 			}
 		}
 	}		
@@ -127,22 +131,24 @@ int main() {
 
 /// READING DATA FROM INPUT FILE
 	FILE* input;
-	input = fopen("docore.txt", "r+");							/// Open input file
+	input = fopen("docore.txt", "r+");												/// Open input file
 
 // #DEFINE PLAYERS
-		gracz red(0,input);								/// Define first hero
-		gracz blue(1,input);								/// Define second hero
+		gracz red(0,input);															/// Define first hero
+		//gracz blue(1,input);														/// Define second hero
 	
 	fclose(input);
 
 /// GENERATING MAP FROM INTERNAL DATA
 	FILE* output;
-	output = fopen("mapka.dat", "rb+");							/// Open map file
+	output = fopen("mapka.dat", "rb+");												/// Open map file
 
 // #GENERATE PLAYERS
 		red.write(output);					
-		blue.write(output);					
+		//blue.write(output);					zn
 
 	fclose(output);
+
+	system("pause");
 		
 }
