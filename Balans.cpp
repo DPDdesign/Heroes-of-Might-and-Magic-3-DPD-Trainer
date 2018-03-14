b0b7902fe52a1e1739f62f649505530bf45189b2
Piotr Musielski <piotrmusielski@mail.com>
2018-03-14 15:15:50 +0100

    Update Balans.cpp

--- Balans.cpp
+++ Balans.cpp
@@ -68,60 +68,106 @@ cout  << n << " " << name[i] << " Attacks " << m << " " << name[j] <<endl <<endl
 int hp1 = hp1s * n;
 int hp2 = hp2s * m; 
 int hp1c = hp1s; int hp2c=hp2s;
+int dr =0;
+int round=1;
 
-while ( m > 0)	{	
 
-		if (att1 > att2)
-		{
+while ( n > 0 &&m > 0)	{	
+
+
+		cout<<"RUNDA: "<<round<<endl<<endl; 
+		
+		if (att1 > att2){
 			double i1 = 0.05 * (att1 - def2);
 				dd = n * dmg1 * (1 + i1);
 		}
-		else
-		{
+		
+		else{
 			double r1 = 0.025 * (def2 - att1);
 				dd = n * dmg1 * (1 - r1);
 		}
 		
-		
-		cout << n <<" " <<name[i]<<" deals: "<< dd << " to " <<name[j]<<endl;
-	
 		if (dd > hp2s){
 			killed = dd/hp2s;
-			hp2c -= dd%hp2s;
+			hp2c = hp2s - dd%hp2c;
+			cout << n		 <<" " 			<<name[i]<<" deals: "<< dd << " to " <<name[j]<<endl;
+			cout << killed 	<< " " 			<< name [j] <<" killed"  << endl;
+			cout << m << " hp left " << hp2c <<  endl<< endl;
+		}
+		
+		else if (dd > hp2c ){
+			killed = 1;
+			hp2c = hp2s - dd%hp2c;
+			cout << n		 <<" " 			<<name[i]<<" deals: "<< dd << " to " <<name[j]<<endl;
+			cout << killed 	<< " " 			<< name [j] <<" killed"  << endl;
+			cout << name [j] << " hp left " << hp2c << endl <<  endl;
+		
+		}	
+			
+		else{
+		hp2c -= dd; killed = 0;
+			cout << n		 <<" " 			<<name[i]<<" deals: "<< dd << " to " <<name[j]<<endl;
+			cout << killed 	<< " " 			<< name [j] <<" killed"  << endl;
+			cout << name [j] << " hp left " << hp2c<< endl <<  endl;
+		}
+	
+	
+	
+		m-=killed;
+	
+	
+	
+	
+	
+	
+		if (att2 > att1){
+			double i1 = 0.05 * (att2 - def1);
+				dd = m * dmg2 * (1 + i1);
 		}
 		
 		else{
-			
-			if (dd > hp2c){
-			killed = 1; }
-			
-			else{hp2c -= hp2s - dd; killed = 0; 	}
-		}
-		
-		m-=killed;	
-		cout << " killed " << killed << endl << " hp left " << hp2c << endl;
-		
-		
-	/*	
-			if (att2 > att1)
-		{
-			double i1 = 0.05 * (att2 - def1);
-				dd = n * dmg1 * (1 + i1);
-		}
-		else
-		{
 			double r1 = 0.025 * (def1 - att2);
-				dd = n * dmg1 * (1 - r1);
+				dd = m * dmg2 * (1 - r1);
 		}
-		killed = dd / hps1;
-		cout << m <<" " <<name[j]<<" deals: "<< dd << " to " <<name[i]<<endl<<endl;
-		n -= killed;
-	*/	
 		
+		if (dd > hp1s){
+			killed = dd/hp1s;
+			hp1c = hp1s - dd%hp1c;
+			cout << m		 <<" " 			<<name[j]<<" deals: "<< dd << " to " <<name[i]<<endl;
+			cout << killed 	<< " " 			<< name [i] <<" killed"  << endl;
+			cout << name [i] << " hp left " << hp1c<< endl <<  endl;
+		}
 		
+		else if (dd > hp1c ){
+			killed = 1;
+			hp1c = hp2s - dd%hp1c;
+			cout << m		 <<" " 			<<name[j]<<" deals: "<< dd << " to " <<name[i]<<endl;
+			cout << killed 	<< " " 			<< name [i] <<" killed"  << endl;
+			cout << name [i] << " hp left " << hp1c<< endl <<  endl;
+		
+		}	
+			
+		else{
+			hp1c -= dd; killed = 0;
+			cout << m		 <<" " 			<<name[j]<<" deals: "<< dd << " to " <<name[i]<<endl;
+			cout << killed 	<< " " 			<< name [i] <<" killed"  << endl;
+			cout << name [i] << " hp left " << hp1c<< endl <<  endl;
+		}
+	
+	
+	
+		n-=killed;
+	
+	round++;	
 	}
 
-
+	if (n <= 0) {
+		cout << "All " << name[id] << " killed! "<<endl<<endl;  }
+	if (m <= 0){
+	cout << "All " << name[di] << " killed! "<<endl<< endl; }
+	
+	cout<<"NASTEPNA WALKA"<<endl<<endl<<"----------------------------------------------------------------------"<<endl<<endl;
+	
 
 } 
 
@@ -129,9 +175,9 @@ int main()
 {
 	stwor troglo(0,4,3,2,5, "Troglo");
 	stwor peasant(1, 1, 1, 1, 1, "Peasant");
-	stwor angel(2, 20, 20, 50, 200, "Archangel");
-	troglo.walka(0,2,100,3);
-	
+	stwor angel(2, 20, 20, 50, 200, "Angel");
+	troglo.walka(0,2,1400,24);
+	troglo.walka(2,0,24,1400);
 	
 
 }
